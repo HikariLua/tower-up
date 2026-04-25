248fc1bda337dcba03bc57fd3f69bb576550eb3e
DESKTOP-67GQGHG\Gustavo <guttycasaes@hotmail.com>
2026-04-25 18:47:26 -0300

    feat: adicionado efeito sonoro de coleta de gema

--- tower-up/entities/collectables/socre_collectable.gd
+++ tower-up/entities/collectables/socre_collectable.gd
@@ -4,16 +4,20 @@ extends Area3D
 @export var score_value: int = 0
 @export var sound_effect: AudioStreamPlayer
 
+var hasCollected: bool = false
+
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	assert(body_entered.is_connected(_on_body_entered))
 	assert(sound_effect)
 
 func _on_body_entered(_body: Node3D) -> void:
-	sound_effect.play()
+	if hasCollected: return 
+	
+	hasCollected = true
 	ScoreAutoload.increase_score(score_value) 
-
-
-
-func _on_sound_finished() -> void:
-	queue_free.call_deferred()
+	$CollisionShape3D.set_deferred("disabled", true)
+	visible=false
+	sound_effect.play()
+	
+	sound_effect.finished.connect(queue_free.call_deferred)
